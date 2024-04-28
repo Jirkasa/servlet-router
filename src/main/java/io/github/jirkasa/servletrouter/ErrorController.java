924db37488718176aa661fd5a43a693767adf0f1
Jiří <satorajirka@seznam.cz>
2024-04-28 18:22:08 +0200

    fix mistake in javadoc

--- src/main/java/io/github/jirkasa/servletrouter/ErrorController.java
+++ src/main/java/io/github/jirkasa/servletrouter/ErrorController.java
@@ -4,7 +4,7 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 
 /**
- * Represents {@link io.github.jirkasa.servletrouter.Controller Controller} that can be set to {@link io.github.jirkasa.servletrouter.Router Router} using {@link io.github.jirkasa.servletrouter.Router#registerErrorController(Class) Router} method. It then catches any exceptions in the router's handle method.
+ * Represents {@link io.github.jirkasa.servletrouter.Controller Controller} that can be set to {@link io.github.jirkasa.servletrouter.Router Router} using {@link io.github.jirkasa.servletrouter.Router#registerErrorController(Class) registerErrorController} method. It then catches any exceptions in the router's handle method.
  * @param <Request> Type of ServletRequest.
  * @param <Response> Type of ServletResponse.
  */
